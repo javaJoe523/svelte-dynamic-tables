f5ce7cc352692a6f6e45eaed6d922bc81c77fed9
Micah Loftin <micahbl@gmail.com>
2023-11-05 01:52:29 -0500

    Fixed the JSON Parser page

--- src/routes/json-parser/+page.svelte
+++ src/routes/json-parser/+page.svelte
@@ -1,26 +1,81 @@
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
-
-<div class="text-column">
-	<h1>About this app</h1>
-
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
-</div>
+<script lang="ts">
+	import Ajv from "ajv";
+  
+	// Define your JSON schema
+	const jsonSchema = {
+	  type: "object",
+	  properties: {
+		// Define your schema properties here
+	  },
+	  required: [],
+	};
+  
+	let jsonString = ''; // Initial JSON string
+	let parsedJson: Record<string, any> | null = null; // Parsed JSON object or null
+	let isValidJson = false; // Flag to track JSON validity
+	let isSchemaValid = false; // Flag to track JSON schema validity
+  
+	// Function to update the parsed JSON and check validity
+	function updateJson() {
+	  try {
+		parsedJson = JSON.parse(jsonString);
+		isValidJson = true;
+  
+		// Validate against the JSON schema
+		const ajv = new Ajv();
+		isSchemaValid = ajv.validate(jsonSchema, parsedJson);
+	  } catch (error) {
+		parsedJson = null;
+		isValidJson = false;
+		isSchemaValid = false;
+	  }
+	}
+  </script>
+  
+  <main>
+	<h1>JSON Parser and Validator</h1>
+  
+	<label for="jsonInput">Enter JSON:</label>
+	<textarea
+	  id="jsonInput"
+	  bind:value={jsonString}
+	  on:input={updateJson}
+	  placeholder="Enter valid JSON here"
+	></textarea>
+  
+	{#if isValidJson}
+	  {#if isSchemaValid}
+		<div class="valid">Valid JSON and Schema:</div>
+		<pre>{JSON.stringify(parsedJson, null, 2)}</pre>
+	  {:else}
+		<div class="invalid">Valid JSON but Invalid Schema</div>
+	  {/if}
+	{:else}
+	  <div class="invalid">Invalid JSON</div>
+	{/if}
+  </main>
+  
+  <style>
+	h1 {
+	  text-align: center;
+	}
+  
+	label {
+	  display: block;
+	  margin-top: 1em;
+	}
+  
+	textarea {
+	  width: 100%;
+	  height: 200px;
+	}
+  
+	.valid {
+	  color: green;
+	}
+  
+	.invalid {
+	  color: red;
+	}
+  </style>
+  
